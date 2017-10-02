16e05f2c9cd5a0ade36e97edb8d27dbb81b02237
yacoub <abeidi.yacoub@gmail.com>
2017-10-02 22:30:09 +0000

    TIM:0012 Nouveau WebService, conversion point de fidélité

--- lib/tbLibelle.php
+++ lib/tbLibelle.php
@@ -116,12 +116,18 @@ try {
     }
 
     $libCorrespondance = array();
-    $req = "SELECT * FROM historique_correspondance ORDER BY h_date ";
+    $req = "SELECT * FROM historique_correspondance ORDER BY type, h_date ";
     $result = $connection->query($req);
     while ($ligne = $result->fetch(PDO::FETCH_OBJ)) {
         $libCorrespondance[$ligne->champ] = $ligne->h_date;
     }
 } catch (PDOException $e) {
     $tbRetour['message'] = $e->getMessage();
+}
+
+
+function createTable($tableName, $connection){
+    $rq = "CREATE TABLE public.$tableName(j1 integer DEFAULT 0,j2 integer DEFAULT 0,j3 integer DEFAULT 0,j4 integer DEFAULT 0,j5 integer DEFAULT 0,j6 integer DEFAULT 0,j7 integer DEFAULT 0,j8 integer DEFAULT 0,j9 integer DEFAULT 0,j11 integer DEFAULT 0,j12 integer DEFAULT 0,j13 integer DEFAULT 0,j14 integer DEFAULT 0,j15 integer DEFAULT 0,j16 integer DEFAULT 0,j17 integer DEFAULT 0,j18 integer DEFAULT 0,j19 integer DEFAULT 0,j20 integer DEFAULT 0,j21 integer DEFAULT 0,j22 integer DEFAULT 0,j23 integer DEFAULT 0,j24 integer DEFAULT 0,j25 integer DEFAULT 0,j26 integer DEFAULT 0,j27 integer DEFAULT 0,j28 integer DEFAULT 0,j29 integer DEFAULT 0,j30 integer DEFAULT 0,j31 integer DEFAULT 0,j32 integer DEFAULT 0,m1 integer DEFAULT 0,m2 integer DEFAULT 0,m3 integer DEFAULT 0,m4 integer DEFAULT 0,m5 integer DEFAULT 0,m6 integer DEFAULT 0,m7 integer DEFAULT 0,m8 integer DEFAULT 0,m9 integer DEFAULT 0,m10 integer DEFAULT 0,m11 integer DEFAULT 0,m12 integer DEFAULT 0,m13 integer DEFAULT 0,a1 bigint DEFAULT 0,a2 bigint DEFAULT 0,a3 bigint DEFAULT 0,a4 bigint DEFAULT 0,a5 bigint DEFAULT 0,numero character varying(15) NOT NULL,CONSTRAINT ".$tableName."_pkey PRIMARY KEY (numero)) WITH (OIDS=FALSE)";
+    $connection->query($rq);
 }
 ?>
