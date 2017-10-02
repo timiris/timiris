c8b50cda71ad8ca13020955200855f7fd31d8f83
yacoub <abeidi.yacoub@gmail.com>
2017-10-02 22:30:46 +0000

    TIM:0012 Nouveau WebService, conversion point de fidélité

--- ws/TimirisServer.php
+++ ws/TimirisServer.php
@@ -8,5 +8,7 @@ $server->register("getFidelitySolde", array('msisdn'=>'xsd:string', 'access'=>'x
 $server->register("subscribeFidelity", array('msisdn'=>'xsd:string', 'access'=>'xsd:string'), array('return'=>'xsd:int') ,$ns);
 $server->register("adjustFidelitySolde", array('msisdn'=>'xsd:string', 'value'=>'xsd:int', 'access'=>'xsd:string'),
         array('return'=>'xsd:string') ,$ns);
+$server->register("buyServiceFidelity", array('msisdn'=>'xsd:string', 'value'=>'xsd:int', 'service_id'=>'xsd:string', 'access'=>'xsd:string'),
+        array('return'=>'xsd:string') ,$ns);
 $server->service(file_get_contents("php://input"));
 ?>
